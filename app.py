5c42f6233864f4d5616fdbec05b0b47e6385f9b5
unknown <mdarshansari3426@gmail.com>
2025-09-06 10:18:42 +0530

    updated app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ if not api_key:
     st.info("Please enter Groq Api key")
     st.stop()
 
-llm=ChatGroq(groq_api_key=api_key,model="Llama3-8b-8192",streaming=True)
+llm=ChatGroq(groq_api_key=api_key,model="llama-3.1-8b-instant",streaming=True)
 
 @st.cache_resource(ttl="2h")
 def configure_db(db_uri,mysql_host=None,mysql_user=None,mysql_password=None,mysql_db=None):
